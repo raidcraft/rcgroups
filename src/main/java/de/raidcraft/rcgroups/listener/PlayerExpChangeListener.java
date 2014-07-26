81fb04d3c193c0917e12dfc526602aa8ba05f4b1
Denis Cundari <denis@cundari.de>
2014-07-26 19:08:00 +0200

    added player exp change listener

---
+++ src/main/java/de/raidcraft/rcgroups/listener/PlayerExpChangeListener.java
@@ -0,0 +1,14 @@
+package de.raidcraft.rcgroups.listener;
+
+import org.bukkit.event.player.PlayerExpChangeEvent;
+
+/**
+ *
+ */
+public class PlayerExpChangeListener {
+
+    private void onPlayerExperienceChange(PlayerExpChangeEvent event) {
+
+
+    }
+}
