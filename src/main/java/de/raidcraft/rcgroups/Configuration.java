080b80ddc356946da058d602a650ed36d1fd4ee3
Denis Cundari <denis@cundari.de>
2015-06-11 19:02:53 +0200

    configuration option for maximum group sizes

--- src/main/java/de/raidcraft/rcgroups/Configuration.java
+++ src/main/java/de/raidcraft/rcgroups/Configuration.java
@@ -1,15 +1,30 @@
 package de.raidcraft.rcgroups;
 
+import de.raidcraft.api.config.Comment;
 import de.raidcraft.api.config.ConfigurationBase;
+import de.raidcraft.api.config.Setting;
 
 /**
  *
  */
 public class Configuration extends ConfigurationBase<GroupsPlugin> {
 
+    public static final String CONFIG_NAME = "config.yml";
+
+    @Setting("size.max.party")
+    @Comment("Maximum party size [0, Integer.MAX_VALUE]")
+    public int maxPartySize = 5;
+
+    @Setting("size.max.raid")
+    @Comment("Maximum raid size [0, Integer.MAX_VALUE]")
+    public int maxRaidSize = 10;
+
+    @Setting("size.max.squad")
+    @Comment("Maximum squad size [0, Integer.MAX_VALUE]")
+    public int maxSquadSize = Integer.MAX_VALUE;
 
     public Configuration(final GroupsPlugin plugin) {
 
-        super(plugin, "config.yml");
+        super(plugin, CONFIG_NAME);
     }
 }
